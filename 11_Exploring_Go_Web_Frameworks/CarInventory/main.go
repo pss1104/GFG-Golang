6274e04d23e5992e1d614f74d7fb4bb4b3c17893
Sai1104 <21bd1a051fcsea@gmail.com>
2025-09-04 19:46:00 +0530

    custom and inbuilt middleware usage with fiber

--- 11_Exploring_Go_Web_Frameworks/CarInventory/main.go
+++ 11_Exploring_Go_Web_Frameworks/CarInventory/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"cars/config"
 	"cars/handlers"
+	middlewares "cars/middleware"
 	"fmt"
 	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/etag"
 	"github.com/gofiber/fiber/v2/middleware/logger" ///this is fibre specific middleware, unlike default gorm/ net-http that get added to the handler chain
 )
 
@@ -16,17 +21,35 @@ func main() {
 	app := fiber.New()
 	app.Use(logger.New())
 
+	//middlewares
+	app.Use(middlewares.SecurityHeaders)
+
+	app.Use(basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"admin": "admin123",
+			"john":  "doe",
+		},
+		Unauthorized: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		},
+	}))
+
+	app.Use(etag.New())
+	app.Use(cors.New())
+
 	// API endpoints:
 	// POST:   http://localhost:5050/cars
 	// GET:    http://localhost:5050/cars/:id
 	// DELETE: http://localhost:5050/cars/:id
 
-	app.Post("/cars",handlers.CreateCar)
-	app.Get("/cars/:id",handlers.GetCar)
-	app.Delete("/cars/:id",handlers.DeleteCar)
+	app.Post("/cars", handlers.CreateCar)
+	app.Get("/cars/:id", handlers.GetCar)
+	app.Delete("/cars/:id", handlers.DeleteCar)
 
 	fmt.Println("Fiber HTTP Server listening on 5050...")
-	if err:=app.Listen(":5050"); err!=nil{
-		log.Fatal("Couldnt listen on port 5050",err)
+	if err := app.Listen(":5050"); err != nil {
+		log.Fatal("Couldnt listen on port 5050", err)
 	}
 }
--- 11_Exploring_Go_Web_Frameworks/CarInventory/middleware/middlewares.go
+++ 11_Exploring_Go_Web_Frameworks/CarInventory/middleware/middlewares.go
@@ -1,26 +1,15 @@
 package middlewares
 
 import (
-	"net/http"
-	"time"
-	"fmt"
+	"github.com/gofiber/fiber/v2"
 )
 
-func Logger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		fmt.Printf("Request received | Path: %v | Method: %v ",r.URL.Path,r.Method)
-		next.ServeHTTP(w, r)
-		fmt.Printf("\nRequest completed in: %v\n",time.Since(start))
-	})
-}
+//we are using the default logger
 
-func SecurityHeaders(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'")
-		w.Header().Set("X-Frame-Options", "DENY")
-		w.Header().Set("X-XSS-Protection", "1; mode=block")
-		next.ServeHTTP(w, r)
-	})
+func SecurityHeaders(c *fiber.Ctx) error {
+	
+	c.Response().Header.Add("Content-Security-Policy", "default-src 'self'")
+	c.Response().Header.Add("X-Frame-Options", "DENY")
+	return c.Next()
 }
 
